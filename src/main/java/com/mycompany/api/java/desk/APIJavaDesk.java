1df423f30e86e2019410ae34367283b6df1d070e
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2025-04-10 19:54:57 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/mycompany/api/java/desk/APIJavaDesk.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.api.java.desk;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author Usuario
+ */
+public class APIJavaDesk {
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
